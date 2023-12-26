b8451d499b01d2cf69fc5a67d9e9ed3689cbfc37
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2023-12-27 00:50:00 +0100

    Update fetchConsolidatedBlockFiles.sh

--- Bash/fetchConsolidatedBlockFiles.sh
+++ Bash/fetchConsolidatedBlockFiles.sh
@@ -14,12 +14,12 @@ base_url="https://blocks.nyzo.co/blockFiles"
 id=0
 
 while true; do
-	if [ $id -lt 1000000 ]; then
+    if [ $id -lt 1000000 ]; then
         formatted_id=$(printf "%06d" $id)
-	else 
-		formatted_id=$id
+    else
+        formatted_id=$id
     fi
-    
+
     output_file="$formatted_id.nyzoblock"
 
     if [ -e "$output_file" ]; then
@@ -35,20 +35,20 @@ while true; do
 
     response=$(curl -s -o "$temp_file" -w "%{http_code}" "$url")
 
-    http_status="${response:0:1}" 
+    http_status="${response:0:1}"
 
     if [[ $http_status -eq 2 ]]; then
-		if { c=$(strings -n 10 < "$temp_file") && echo "$c" | grep -q "error reading file"; }; then
+        if { c=$(strings -n 10 <"$temp_file") && echo "$c" | grep -q "error reading file"; }; then
             echo "200 - error reading file for $output_file - the consolidated block may not be available yet - retrying in 60 seconds..."
             rm -f "$temp_file"
-			sleep 60
+            sleep 60
             continue
         fi
-		
+
         echo "200 for $output_file, writing to file"
         mv "$temp_file" "$output_file"
         ((id++))
-		sleep 0.2
+        sleep 0.2
     else
         echo "received non-2xx HTTP status code - retrying in 20 seconds..."
         rm -f "$temp_file"
