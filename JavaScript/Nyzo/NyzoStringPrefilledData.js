26d19196d1c595544ccf209d1423fea886d9c80a
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-01-19 03:33:15 +0100

    Create NyzoStringPrefilledData.js

---
+++ JavaScript/Nyzo/NyzoStringPrefilledData.js
@@ -0,0 +1,14 @@
+class NyzoStringPrefilledData {
+    constructor(receiverIdentifier, senderData) {
+        this.receiverIdentifier = receiverIdentifier;
+        this.senderData = senderData;
+    }
+
+    GetReceiverIdentifier() {
+        return this.receiverIdentifier;
+    }
+
+    GetSenderData() {
+        return this.senderData;
+    }
+}
