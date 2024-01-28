da3f758b1fed4bf123fe106def850dc20adc68c9
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-01-28 17:18:52 +0100

    Create NyzoConstants.js

---
+++ JavaScript/Nyzo/NyzoConstants.js
@@ -0,0 +1,25 @@
+class NyzoConstants {
+    static GetGenesisBlockHash(){
+        return NyzoUtil.HexStringAsUint8Array("bc4cca2a2a50a229-256ae3f5b2b5cd49-aa1df1e2d0192726-c4bb41cdcea15364");
+    }
+
+    static GetMicroNyzosPerNyzo(){
+        return 1000000;
+    }
+
+    static GetMaximumSenderDataLength(){ 
+        return 32;
+    }
+
+    static GetMinimumTransactionAmount(){
+        let microNyzosPerNyzo = NyzoConstants.GetMicroNyzosPerNyzo().toString();
+        let length = microNyzosPerNyzo.length - 1;
+        let result = "0.";
+
+        for(let i=1; i <= length; i++){
+            result += ((i == length) ? "1" : "0");
+        }
+
+        return parseFloat(result);
+    }
+}
