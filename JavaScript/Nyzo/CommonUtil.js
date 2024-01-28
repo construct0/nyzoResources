76e81deb3bb856b81fafbbf821ea3fa6a2e0ddf1
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-01-28 17:18:50 +0100

    Create CommonUtil.js

---
+++ JavaScript/Nyzo/CommonUtil.js
@@ -0,0 +1,7 @@
+class CommonUtil {
+
+    // undefined is a built-in JavaScript keyword and can be overridden, this assures consistency
+    static IsUndefined(value){
+        return value === void(0);
+    }
+}
