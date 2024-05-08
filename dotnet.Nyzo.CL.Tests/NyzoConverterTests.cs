81754af1015e2b6ba705689c9b4ca05d9e610bde
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-05-08 20:24:06 +0200

    Update NyzoConverterTests.cs

--- dotnet.Nyzo.CL.Tests/NyzoConverterTests.cs
+++ dotnet.Nyzo.CL.Tests/NyzoConverterTests.cs
@@ -8,7 +8,7 @@ public class NyzoConverterTests {
 	private List<CryptographicInlineData> _vectors { get; init; } = new SodiumPublicKeyAuthTests().Ed25519_RFC8032_TestVectors;
 
 	[Fact]
-	public void PublicIdentifierForPrivateKey_ShouldReturnInstance_OrNull() {
+	public void PublicIdentifierForPrivateKey_ShouldReturnInstance() {
 		foreach(var vector in _vectors) {
 			Assert.Equal(vector.NyzoStringPublicIdentifier, NyzoConverter.PublicIdentifierForPrivateKey(vector.NyzoStringPrivateKey!));
 		}
@@ -20,5 +20,12 @@ public class NyzoConverterTests {
 
 		Assert.EndsWith("0.000001", NyzoConverter.GetDisplayAmount(amount, true));
 		Assert.EndsWith("1.000000", NyzoConverter.GetDisplayAmount(amount, false));
+	}
+
+	[Fact]
+	public void GetAmountOfMicroNyzos_ShouldBeAccurate() {
+		Assert.Equal(0, NyzoConverter.GetAmountOfMicroNyzos("0.0000005"));
+		Assert.Equal(1, NyzoConverter.GetAmountOfMicroNyzos("0.000001"));
+		Assert.Equal(100_000_000__000_000, NyzoConverter.GetAmountOfMicroNyzos("100000000"));
 	}
 }
