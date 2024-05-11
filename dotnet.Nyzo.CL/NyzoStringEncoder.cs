38aff7a8349581768dcf55cf293c855f39883298
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-05-11 02:15:33 +0200

    dotnet.Nyzo.CL - Update NyzoStringEncoder.cs

--- dotnet.Nyzo.CL/NyzoStringEncoder.cs
+++ dotnet.Nyzo.CL/NyzoStringEncoder.cs
@@ -5,7 +5,11 @@ using System.Text.RegularExpressions;
 
 namespace Nyzo.CL;
 
-public static class NyzoStringEncoder {
+/// <summary>
+/// <para>Methods which aren't intended to be publicly accessible have been assigned the "protected" keyword, a shim class then makes the methods accessible internally within the Nyzo.CL.Tests namespace (i.e. ShimNyzoStringEncoder)</para>
+/// <para>For the same reason the class is not marked as static, to enable inheritance.</para>
+/// </summary>
+public class NyzoStringEncoder {
     public static char[] CharacterLookup => ("0123456789" + "abcdefghijkmnopqrstuvwxyz" + "ABCDEFGHIJKLMNPQRSTUVWXYZ" + "-.~_").ToArray();
 
     public static Dictionary<char, int> CharacterToValueDict {get {
@@ -20,7 +24,7 @@ public static class NyzoStringEncoder {
     }}
 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/subarray
-    private static byte[] ProvideSubArray(byte[] input, int inclusiveBegin, int exclusiveEnd){
+    protected static byte[] ProvideSubArray(byte[] input, int inclusiveBegin, int exclusiveEnd){
         int subArrayLength = exclusiveEnd - inclusiveBegin;
         byte[] subArray = new byte[subArrayLength];
         Array.Copy(input, inclusiveBegin, subArray, 0, subArrayLength);
@@ -28,8 +32,14 @@ public static class NyzoStringEncoder {
         return subArray;
     }
 
-    // Null if this fails, after which the other Decode[..] functions below also return null
-    private static byte[]? DecodeToContentBytes(string encodedString){
+	/// <summary>
+	/// <para>Converts the NyzoString encoded string back to a byte[]. This byte array can in turn be converted back to a hex string and represents the legacy pub/priv format.</para>
+	/// <para>It is imperative that you do not reference this method directly but use one of the public Decode methods of this class.</para>
+	/// <para><see href="https://tech.nyzo.org/dataFormats/nyzoString">View specification.</see></para>
+	/// </summary>
+	/// <param name="encodedString">Any NyzoString encoded string.</param>
+	/// <returns>byte[] (valid checksum) or null (invalid checksum)</returns>
+	protected static byte[]? DecodeToContentBytes(string encodedString){
         byte[]? contentBytes = null;
         encodedString = encodedString.Trim();
 
@@ -42,73 +52,107 @@ public static class NyzoStringEncoder {
         encodedString = Regex.Replace(encodedString, "/l/g", "1");
         encodedString = Regex.Replace(encodedString, "/O/g", "0");
 
-        // Get the prefix
-        var prefix = encodedString.Substring(0, 4);
-
         // Get the array representation of the encoded string
-        byte[] expandedArray = NyzoStringEncoder.ByteArrayForEncodedString(encodedString);
+        byte[]? expandedArray = NyzoStringEncoder.ByteArrayForEncodedString(encodedString);
 
-        // Get the content length from the next byte and calculate the checksum length
-        var contentLength = expandedArray[3] & 0xff;
-        var checksumLength = expandedArray.Length - contentLength - 4;
-
-        // Return null if the checksum length is invalid
-        if(!(checksumLength >= 4 && checksumLength <= 6)){
+        // A character was provided which is not part of the character lookup
+        if(expandedArray is null) {
             return null;
         }
 
-        // The checksum length is valid
-        // Calculate the checksum and compare it to the provided checksum. Only create the result array if the checksums match
-        var headerLength = 4;
+		// Get the content length from the next byte and calculate the checksum length
+		var contentLength = expandedArray[3] & 0xff;
+		var checksumLength = expandedArray.Length - contentLength - 4;
 
-        var calculatedChecksum =
-            NyzoStringEncoder.ProvideSubArray(
-                NyzoUtil.ByteArrayAsDoubleSha256ByteArray(
-                    NyzoStringEncoder.ProvideSubArray(expandedArray, 0, headerLength + contentLength)
-                ),
-                0,
-                checksumLength
-            );
+		// Return null if the checksum length is invalid
+		if (!(checksumLength >= 4 && checksumLength <= 6)) {
+			return null;
+		}
 
-        var providedChecksum = NyzoStringEncoder.ProvideSubArray(expandedArray, expandedArray.Length - checksumLength, expandedArray.Length);
+		// The checksum length is valid
+		// Calculate the checksum and compare it to the provided checksum. Only create the result array if the checksums match
+		var headerLength = 4;
 
-        if(calculatedChecksum.SequenceEqual(providedChecksum)){
-            // Get the content array. This is the encoded object with the prefix, length byte, and checksum removed
-            contentBytes = NyzoStringEncoder.ProvideSubArray(expandedArray, headerLength, expandedArray.Length - checksumLength);
+		var calculatedChecksum =
+			NyzoStringEncoder.ProvideSubArray(
+				NyzoUtil.ByteArrayAsDoubleSha256ByteArray(
+					NyzoStringEncoder.ProvideSubArray(expandedArray, 0, headerLength + contentLength)
+				),
+				0,
+				checksumLength
+			);
+
+		var providedChecksum = NyzoStringEncoder.ProvideSubArray(expandedArray, expandedArray.Length - checksumLength, expandedArray.Length);
+
+		if (calculatedChecksum.SequenceEqual(providedChecksum)) {
+			// Get the content array. This is the encoded object with the prefix, length byte, and checksum removed
+			contentBytes = NyzoStringEncoder.ProvideSubArray(expandedArray, headerLength, expandedArray.Length - checksumLength);
+		}
+
+		return contentBytes;
+
+
+	}
+
+	/// <summary>
+	/// <para>Converts the NyzoString encoded private key back to a byte[]. This byte array can in turn be converted back to a hex string and represents the legacy pub/priv format.</para>
+	/// <para><see href="https://tech.nyzo.org/dataFormats/nyzoString">View specification.</see></para>
+	/// </summary>
+	/// <param name="encodedString">A NyzoString encoded private key string.</param>
+	/// <returns>byte[] (valid checksum) or null (invalid checksum)</returns>
+	public static NyzoStringPrivateKey? DecodePrivateKey(string encodedString){
+        // An additional prefix validation is performed
+        if(!(encodedString.Length >= 4)) {
+            return null;
         }
 
-        return contentBytes;
-    }
-
-    public static NyzoStringPrivateSeed? DecodePrivateSeed(string encodedString){
-        // An additional prefix validation is performed
-        if(!(encodedString.Length >= 4 || encodedString[..4] == "key_")){
+        if (!(encodedString[..4] == NyzoStringPrefix.PrivateKey)) {
             return null;
         }
 
         var contentBytes = NyzoStringEncoder.DecodeToContentBytes(encodedString);
 
-        return contentBytes is null ? null : new NyzoStringPrivateSeed(contentBytes);
+        return contentBytes is null ? null : new NyzoStringPrivateKey(contentBytes);
     }
 
-    public static NyzoStringPublicIdentifier? DecodePublicIdentifier(string encodedString){
-        // An additional prefix validation is performed
-        if(!(encodedString.Length >= 4 || encodedString[..4] == "id__")){
-            return null;
-        }
+	/// <summary>
+	/// <para>Converts the NyzoString encoded public identifier back to a byte[]. This byte array can in turn be converted back to a hex string and represents the legacy pub/priv format.</para>
+	/// <para><see href="https://tech.nyzo.org/dataFormats/nyzoString">View specification.</see></para>
+	/// </summary>
+	/// <param name="encodedString">A NyzoString encoded public identifier string.</param>
+	/// <returns>byte[] (valid checksum) or null (invalid checksum)</returns>
+	public static NyzoStringPublicIdentifier? DecodePublicIdentifier(string encodedString){
+		// An additional prefix validation is performed
+		if (!(encodedString.Length >= 4)) {
+			return null;
+		}
 
-        var contentBytes = NyzoStringEncoder.DecodeToContentBytes(encodedString);
+		if (!(encodedString[..4] == NyzoStringPrefix.PublicIdentifier)) {
+			return null;
+		}
+
+		var contentBytes = NyzoStringEncoder.DecodeToContentBytes(encodedString);
 
         return contentBytes is null ? null : new NyzoStringPublicIdentifier(contentBytes);
     }
 
-    public static NyzoStringPrefilledData? DecodePrefilledData(string encodedString){
-        // An additional prefix validation is performed
-        if(!(encodedString.Length >= 4 || encodedString[..4] == "pre_")){
-            return null;
-        }
+	/// <summary>
+	/// <para>Converts the NyzoString encoded prefilled data back to a byte[]. This byte array can in turn be converted back to a hex string and represents the legacy pub/priv format.</para>
+	/// <para><see href="https://tech.nyzo.org/dataFormats/nyzoString">View specification.</see></para>
+	/// </summary>
+	/// <param name="encodedString">A NyzoString encoded prefilled data string.</param>
+	/// <returns>byte[] (valid checksum) or null (invalid checksum)</returns>
+	public static NyzoStringPrefilledData? DecodePrefilledData(string encodedString){
+		// An additional prefix validation is performed
+		if (!(encodedString.Length >= 4)) {
+			return null;
+		}
 
-        var contentBytes = NyzoStringEncoder.DecodeToContentBytes(encodedString);
+		if (!(encodedString[..4] == NyzoStringPrefix.PrefilledData)) {
+			return null;
+		}
+
+		var contentBytes = NyzoStringEncoder.DecodeToContentBytes(encodedString);
 
         return contentBytes is null ? null : new NyzoStringPrefilledData(
             NyzoStringEncoder.ProvideSubArray(contentBytes, 0, 32),
@@ -116,38 +160,48 @@ public static class NyzoStringEncoder {
         );
     }
 
-    public static NyzoTransaction? DecodeNyzoTransaction(string encodedString){
-        // An additional prefix validation is performed
-        if(!(encodedString.Length >= 4 || encodedString[..4] == "tx__")){
-            return null;
-        }
+	/// <summary>
+	/// <para>Converts the NyzoString encoded transaction back to a byte[]. This byte array can in turn be converted back to a hex string and represents the legacy pub/priv format.</para>
+	/// <para><see href="https://tech.nyzo.org/dataFormats/nyzoString">View specification.</see></para>
+	/// </summary>
+	/// <param name="encodedString">A NyzoString encoded transaction string.</param>
+	/// <returns>byte[] (valid checksum) or null (invalid checksum)</returns>
+	public static NyzoTransaction? DecodeNyzoTransaction(string encodedString){
+		// An additional prefix validation is performed
+		if (!(encodedString.Length >= 4)) {
+			return null;
+		}
 
-        var contentBytes = NyzoStringEncoder.DecodeToContentBytes(encodedString);
+		if (!(encodedString[..4] == NyzoStringPrefix.Transaction)) {
+			return null;
+		}
+
+		var contentBytes = NyzoStringEncoder.DecodeToContentBytes(encodedString);
 
         return contentBytes is null ? null : NyzoTransaction.FromBytes(contentBytes);
     }
 
-    public static byte[] ByteArrayForEncodedString(string encodedString){
+    private static byte[]? ByteArrayForEncodedString(string encodedString){
         var characterToValueDict = NyzoStringEncoder.CharacterToValueDict;
 
-        var arrayLength = (encodedString.Length * 6 + 7) / 8;
-        var array = new byte[arrayLength];
+		var arrayLength = (encodedString.Length * 6 + 7) / 8;
+		var array = new byte[arrayLength];
 
-        for(var i=0; i<arrayLength; i++){
-            var leftCharacter = encodedString[i * 8 / 6];
-            var rightCharacter = encodedString[i * 8 / 6 + 1];
+		for (var i = 0; i<arrayLength; i++) {
+			var leftCharacter = encodedString[i * 8 / 6];
+			var rightCharacter = encodedString[i * 8 / 6 + 1];
 
-            var leftValue = characterToValueDict[leftCharacter];
-            var rightValue = characterToValueDict[rightCharacter];
-            var bitOffset = (i * 2) % 6;
+			var leftValue = characterToValueDict[leftCharacter];
+			var rightValue = characterToValueDict[rightCharacter];
+			var bitOffset = (i * 2) % 6;
 
-            array[i] = (byte)((((leftValue << 6) + rightValue) >> 4 - bitOffset) & 0xff);
-        }
+			array[i] = (byte)((((leftValue << 6) + rightValue) >> 4 - bitOffset) & 0xff);
+		}
 
-        return array;
-    }
+		return array;
+	}
 
-    public static string EncodedStringForByteArray(byte[] array){
+    private static string EncodedStringForByteArray(byte[] array){
         var characterLookup = NyzoStringEncoder.CharacterLookup;
 
         var index = 0;
@@ -175,9 +229,10 @@ public static class NyzoStringEncoder {
         return encodedString;
     }
 
-    private static string EncodedNyzoString(string prefix, byte[] contentBytes){
+    protected static string EncodedNyzoString(string prefix, byte[] contentBytes){
         // Get the prefix array from the type
-        var prefixBytes = NyzoStringEncoder.ByteArrayForEncodedString(prefix);
+        // We do not expect a null to be returned due to the prefix being provided by the calling methods within this class
+        byte[] prefixBytes = NyzoStringEncoder.ByteArrayForEncodedString(prefix)!;
 
         // Determine the length of the expanded array with the header and the checksum. The header is the type-specific prefix in characters followed by a single byte that indicates the length of the content array (four bytes total). The checksum is a minimum of 4 bytes and a maximum of 6 bytes, widening the expanded array so that its length is divisible by 3.
         var checksumLength = 4 + (3 - (contentBytes.Length + 2) % 3) % 3;
@@ -208,7 +263,7 @@ public static class NyzoStringEncoder {
         return NyzoStringEncoder.EncodedStringForByteArray(expandedArray);
     }
 
-    public static string NyzoStringFromPrivateKey(byte[] array){
+    public static string NyzoStringFromPrivateSeed(byte[] array){
         return NyzoStringEncoder.EncodedNyzoString("key_", array);
     }
 
