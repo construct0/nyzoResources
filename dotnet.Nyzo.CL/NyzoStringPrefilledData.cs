9d1878ee5c0965aca56dc4ae4254d5caeef8da68
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-05-11 02:15:18 +0200

    dotnet.Nyzo.CL - Update NyzoStringPrefilledData.cs

--- dotnet.Nyzo.CL/NyzoStringPrefilledData.cs
+++ dotnet.Nyzo.CL/NyzoStringPrefilledData.cs
@@ -1,12 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Nyzo.CL;
 
 public class NyzoStringPrefilledData {
     public byte[] ReceiverIdentifier {get;init;}
     public byte[] SenderData {get;init;}
 
-    public NyzoStringPrefilledData(byte[] receiverIdentifier, byte[] senderData){
+	// todo: add Amount; the nyzoVerifier.NyzoStringPrefilledData class differs from the ported nyzoChromeExtension JS class
+	// todo: add "Amount" to spec @ https://tech.nyzo.org/dataFormats/nyzoString
+
+	public NyzoStringPrefilledData(byte[] receiverIdentifier, byte[] senderData){
         this.ReceiverIdentifier = receiverIdentifier;
         this.SenderData = senderData;
+    }
+
+    // TODO: FieldByteSize, cf NyzoTransaction decode test
+    public byte[] GetBytes() {
+        byte[] array = new byte[32 + 1 + this.SenderData.Length + (0)];
+        var buffer = new ByteBuffer(array);
+
+        buffer.PutBytes(this.ReceiverIdentifier);
+
+        buffer.PutByte((byte)this.SenderData.Length);
+
+        buffer.PutBytes(this.SenderData);
+
+        return buffer.ReadBytes();
     }
 }
 
