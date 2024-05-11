6e8491c9b1c464b9a35604f9c6001bb5247ec4a8
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-05-11 02:15:51 +0200

    dotnet.Nyzo.CL.Tests - Create ShimNyzoStringEncoder.cs

---
+++ dotnet.Nyzo.CL.Tests/internal/ShimNyzoStringEncoder.cs
@@ -0,0 +1,21 @@
+﻿namespace Nyzo.CL.Tests.@internal;
+
+/// <summary>
+/// Methods in NyzoStringEncoder which aren't intended to be publicly accessible have been assigned the "protected" keyword, this shim class then makes the method accessible internally, i.e. within the Nyzo.CL.Tests namespace
+/// </summary>
+internal sealed class ShimNyzoStringEncoder : NyzoStringEncoder
+{
+    public static byte[] _ProvideSubArray(byte[] input, int inclusiveBegin, int exclusiveEnd)
+    {
+        return ProvideSubArray(input, inclusiveBegin, exclusiveEnd);
+    }
+
+    public static byte[]? _DecodeToContentBytes(string encodedString)
+    {
+        return DecodeToContentBytes(encodedString);
+    }
+
+    public static string _EncodedNyzoString(string prefix, byte[] contentBytes) {
+        return EncodedNyzoString(prefix, contentBytes);
+    }
+}
